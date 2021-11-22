79abea4497f0315998cf4e9789cace0a57e544e7
Md Shamim Hossain <37259975+shamim-ice@users.noreply.github.com>
2021-11-22 23:44:04 +0600

    Add files via upload

---
+++ Energy_detector.py
@@ -0,0 +1,12 @@
+from Energy_detector_a import main as main_a
+from Energy_detector_b import main as main_b
+from Energy_detector_c import main as main_c
+
+print("Executing Problem 1A")
+main_a()
+
+print("Executing Problem 1B")
+main_b()
+
+print("Executing Problem 1C")
+main_c()
