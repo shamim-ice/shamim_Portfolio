21f11a7a236397c52462a4f7c2e0024485ecd240
Md Shamim Hossain <37259975+shamim-ice@users.noreply.github.com>
2021-12-14 10:57:01 +0600

    Update DecisionTREE.py

--- ML micro Projects in python/DecisionTREE.py
+++ ML micro Projects in python/DecisionTREE.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 from pprint import pprint
 
+
+#This dataset consists of 101 animals from a zoo. There are 16 variables with various traits to describe the animals.
+#The 7 Class Types are: Mammal, Bird, Reptile, Fish, Amphibian, Bug and Invertebrate.
+#The purpose for this dataset is to be able to predict the classification of the animals, based upon the variables. 
+#It is the perfect dataset for those who are new to learning Machine Learning.
 dataset = pd.read_csv("zoo.csv")
 dataset = dataset.drop('animal_name', axis=1)
 
