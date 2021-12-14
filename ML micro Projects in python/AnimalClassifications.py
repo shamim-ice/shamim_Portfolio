65a05de17c8fbdfbd52577e31b4531467b37fe54
Md Shamim Hossain <37259975+shamim-ice@users.noreply.github.com>
2021-12-14 10:56:04 +0600

    Update AnimalClassifications.py

--- ML micro Projects in python/AnimalClassifications.py
+++ ML micro Projects in python/AnimalClassifications.py
@@ -7,6 +7,10 @@ from sklearn.metrics import classification_report
 
 
 def importdata():
+    #This dataset consists of 101 animals from a zoo. There are 16 variables with various traits to describe the animals.
+    #The 7 Class Types are: Mammal, Bird, Reptile, Fish, Amphibian, Bug and Invertebrate.
+    #The purpose for this dataset is to be able to predict the classification of the animals, based upon the variables. 
+    #It is the perfect dataset for those who are new to learning Machine Learning.
     data = pd.read_csv("zoo.csv")
     data = data.drop('animal_name', axis=1)
     return data
